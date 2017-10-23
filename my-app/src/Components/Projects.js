7ca5d37d5f48d2c17d424b15a82b59e16dacca5e
KB-Spanish-Castle <kylegrantb@gmail.com>
2017-10-22 21:29:54 -0600

    React Practice Video

---
+++ my-app/src/Components/Projects.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+// https://www.youtube.com/watch?v=A71aqufiNtQ
+class Projects extends Component {
+  render() {
+    return (
+      <div className="Projects">
+        My Projects
+       </div>
+    );
+  }
+}
+
+export default Projects;
