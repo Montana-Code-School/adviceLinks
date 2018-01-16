85aefb9fbfc569040e6fd6bb05b211ac839f32fc
KB-Spanish-Castle <kylegrantb@gmail.com>
2018-01-15 23:51:06 -0700

    I fixed a lot of inactive links by moving the misplaced text into the links.

--- src/Freeware/FreewareGames.js
+++ src/Freeware/FreewareGames.js
@@ -6,8 +6,8 @@ class FrewareGames extends Component {
     return (
       <div className="Freeware-main">
         <h1>Freeware Games</h1>
-        <p><a href='https://www.locomalito.com/'></a>Arcade inspired Free Games</p>
-        <p><a href='https://itch.io/'></a>Freeware Games from Itchio</p>
+        <p><a href='https://www.locomalito.com/'>Arcade inspired Free Games</a></p>
+        <p><a href='https://itch.io/'>Freeware Games from Itchio</a></p>
         <p><a href='https://gamejolt.com/'>GameJolt</a></p>
         <p><a href='http://www.indiedb.com/'>IndieDB</a>: are free Games</p>
         <p><a href='https://www.artsoft.org/'>Rocks N' Diamonds</a></p>
@@ -15,17 +15,17 @@ class FrewareGames extends Component {
         <p><a href='http://forum.caravelgames.com/downloads.php'>Deadly Rooms Of Death Architect Edition</a></p>
         <p>Free Blizzard Games for: <a href='https://us.battle.net/account/download/?show=classic'>Windows & Mac</a></p>
         <p>This game dosn't work on modern windows only non 64-bit Windows operating systms and any Linux that runs with wine: <a href='https://archive.org/details/win3_dranstrm'>Drain Storm</a></p>
-        <p><a href='https://osgameclones.com/'></a>Inspired Games</p>
+        <p><a href='https://osgameclones.com/'>Free Inspired Game Projects</a></p>
         <h1>Game Engines</h1>
-        <p><a href='https://developer.android.com/studio/index.html'></a>Android Studio</p>
-        <p><a href='https://www.youtube.com/watch?v=OojQitoAEXs&list=PL2xjPbQaM7JZ_FmXwTAesiAciHEPlGmiW'>Android Studio</a></p>
+        <p><a href='https://developer.android.com/studio/index.html'>Android Studio</a></p>
+        <p><a href='https://www.youtube.com/watch?v=OojQitoAEXs&list=PL2xjPbQaM7JZ_FmXwTAesiAciHEPlGmiW'>Game Tutorials for Android Studio</a></p>
         <p><a href='https://github.com/TheMightyFingers/mightyeditor'>Mighty Editor</a> includes Phaser in a easier interface</p>
         <p><a href='https://www.youtube.com/watch?v=ngvtZZGbwaI'>13 free Open Source software to make your games!</a></p>
         <p><a href='http://cocos2d.org/'>Cocos2D</a></p>
-        <p><a href='http://game-editor.com/Main_Page'></a></p>
+        <p><a href='http://game-editor.com/Main_Page'>Game Editor</a></p>
         <p><a href='https://www.defold.com/'>Defold</a></p>
         <p><a href='http://www.openfl.org/'></a></p>
-        <p><a href='http://www.clickteam.com/download-centre/retired-products'></a>The Games Factory 1.06 & More</p>
+        <p><a href='http://www.clickteam.com/download-centre/retired-products'>The Games Factory 1.06 & More</a></p>
         <p><a href='http://www.clickteam.com/'>Click Team Fusion</a></p>
         <h1>Game Art Tools:</h1>
         <p>A Pixel Art tool: <a href='https://www.aseprite.org/older-versions/'>Aseprite</a></p>
