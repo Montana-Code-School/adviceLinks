ab5c8b7e10e37006d8eee0acf820eeac9eacee34
KB-Spanish-Castle <kylegrantb@gmail.com>
2018-01-16 00:26:09 -0700

    More links added

--- src/Freeware/FreewareGames.js
+++ src/Freeware/FreewareGames.js
@@ -12,9 +12,13 @@ class FrewareGames extends Component {
         <p><a href='http://www.indiedb.com/'>IndieDB</a>: are free Games</p>
         <p><a href='https://www.artsoft.org/'>Rocks N' Diamonds</a></p>
         <p><a href='http://www.openra.net/'>Open Red Alert</a></p>
+        <p><a href='http://scummvm.org/downloads/'>Multiple Platforms</a> to work with multiple <a href='http://scummvm.org/games/'>Free Games</a></p>
+        <p><a href='http://www.raceintospace.org/'>Race Into Space</a></p>
         <p><a href='http://forum.caravelgames.com/downloads.php'>Deadly Rooms Of Death Architect Edition</a></p>
         <p>Free Blizzard Games for: <a href='https://us.battle.net/account/download/?show=classic'>Windows & Mac</a></p>
+        <p><a href='http://www.enemynations.com/'>Enemy Nations</a>: I will add further instructions to get it running.</p>
         <p>This game dosn't work on modern windows only non 64-bit Windows operating systms and any Linux that runs with wine: <a href='https://archive.org/details/win3_dranstrm'>Drain Storm</a></p>
+        <p><a href='https://te4.org/'>Tales of Maj'Eyal</a> is on all three <a href='https://te4.org/download'>Platforms (Win+Mac+Linux)</a></p>
         <p><a href='https://osgameclones.com/'>Free Inspired Game Projects</a></p>
         <h1>Game Engines</h1>
         <p><a href='https://developer.android.com/studio/index.html'>Android Studio</a></p>
@@ -27,6 +31,9 @@ class FrewareGames extends Component {
         <p><a href='http://www.openfl.org/'></a></p>
         <p><a href='http://www.clickteam.com/download-centre/retired-products'>The Games Factory 1.06 & More</a></p>
         <p><a href='http://www.clickteam.com/'>Click Team Fusion</a></p>
+        <p><a href='https://xenko.com/'>Xenko</a></p>
+        <p>This is a interactice Choose your own adventures to create games on multiple platforms: <a href='https://www.renpy.org/'>Ren'Py</a></p>
+        <p>A basic Choose Your Own Adventures with only text and multiple choices to make: <a href='https://www.choiceofgames.com/make-your-own-games/choicescript-intro/'>ChoiceScript Intro</a> and here is the <a href='https://github.com/dfabulich/choicescript'>GitHub</a> Repository</p>
         <h1>Game Art Tools:</h1>
         <p>A Pixel Art tool: <a href='https://www.aseprite.org/older-versions/'>Aseprite</a></p>
         <p><a href='https://www.designer.io/'>Gravit Designer</a> and the <a href='https://www.youtube.com/watch?v=eIgcwY_DGCo'>YouTube-Short-Tutorial(8:58)</a>.</p>
