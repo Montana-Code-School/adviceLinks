228e26666d8c34e6bca75e16d73db31db8b4ee82
KB-Spanish-Castle <kylegrantb@gmail.com>
2018-01-17 17:33:26 -0700

    Additional links added

--- src/Freeware/FreewareGames.js
+++ src/Freeware/FreewareGames.js
@@ -7,37 +7,59 @@ class FrewareGames extends Component {
       <div className="Freeware-main">
         <h1>Freeware Games</h1>
         <p><a href='https://www.locomalito.com/'>Arcade inspired Free Games</a></p>
+        <p>Super Crate Box <a href='http://www.supercratebox.com/'>Offical Website</a> or <a href='http://store.steampowered.com/app/212800/Super_Crate_Box/'>Steam</a></p>
         <p><a href='https://itch.io/'>Freeware Games from Itchio</a></p>
         <p><a href='https://gamejolt.com/'>GameJolt</a></p>
         <p><a href='http://www.indiedb.com/'>IndieDB</a>: are free Games</p>
         <p><a href='https://www.artsoft.org/'>Rocks N' Diamonds</a></p>
-        <p><a href='http://www.openra.net/'>Open Red Alert</a></p>
         <p><a href='http://scummvm.org/downloads/'>Multiple Platforms</a> to work with multiple <a href='http://scummvm.org/games/'>Free Games</a></p>
         <p><a href='http://www.raceintospace.org/'>Race Into Space</a></p>
         <p><a href='http://forum.caravelgames.com/downloads.php'>Deadly Rooms Of Death Architect Edition</a></p>
-        <p>Free Blizzard Games for: <a href='https://us.battle.net/account/download/?show=classic'>Windows & Mac</a></p>
-        <p><a href='http://www.enemynations.com/'>Enemy Nations</a>: I will add further instructions to get it running.</p>
+        <p>Free Blizzard Games including StarCraft Legacy for: <a href='https://us.battle.net/account/download/?show=classic'>Windows & Mac</a></p>
+        <p><a href='http://www.enemynations.com/'>Enemy Nations</a>: The hard way is following these <a href='http://yoni0505.blogspot.com/2013/03/how-to-install-and-run-enemy-nations-on.html'>Instructions</a></p>
+        <p>It is easier to just use a virtual machine of your choice such as VMware Player or Virtual Box and use <a href='http://www.trustfm.net/software/utilities/Folder2Iso.php'>Folder2Iso</a> tool.
         <p>This game dosn't work on modern windows only non 64-bit Windows operating systms and any Linux that runs with wine: <a href='https://archive.org/details/win3_dranstrm'>Drain Storm</a></p>
+        <p><a href='http://www.daggert.net/Folio/Programming/Presage/LodeRunner/Loderunner1.htm'>Lode Runner Win 95</a></p>
+        <p><a href='http://www.factor5.de/downloads.shtml'>Three Amiga Games</a> that works best with <a href='https://fs-uae.net/'>FS-UAE</a> also each time before starting change the option</p>
         <p><a href='https://te4.org/'>Tales of Maj'Eyal</a> is on all three <a href='https://te4.org/download'>Platforms (Win+Mac+Linux)</a></p>
         <p><a href='https://osgameclones.com/'>Free Inspired Game Projects</a></p>
+        <h1>Games that were Commerical Became Offical Freeware</h1>
+        <p><a href='https://archive.org/details/cart-life-v1.6'>CartLife</a> and the source code at: <a href='https://github.com/gondur/cartlife_src'>GitHub</a></p>
+        <p><a href='https://github.com/akarnokd/open-ig'>Open Imperium Galactica</a></p>
+        <p><a href='http://www.amuletsandarmor.com/'>Amulets & Armor</a></p>
+        <p>Because of the early Command & Conquer games were released for free a project was made with an easier interface: <a href='http://www.openra.net/'>Open Red Alert</a></p>
+        <p>Additonal Links for offial released <a href='https://cncnz.com/features/freeware-classic-command-conquer-games/'>Freeware Classics</a> and <a href='https://cncnet.org/'>Mods</a></p>
+        <p><a href='https://www.tribesuniverse.com/'>Tribes Series</a></p>
+        <p><a href='http://en.uesp.net/wiki/Arena:Files'>Elder Scrolls: Arena</a></p>
+        <p><a href='http://en.uesp.net/wiki/Daggerfall:Files'>Elder Scrolls II: Daggerfall</a></p>
+        <p>7 Kingdomes Ancient Adversaries is a realtime strategy mixed with civilzation mechanics including researching: <a href='https://github.com/the3dfxdude/7kaa/releases'>GitHub</a> <a href='https://7kfans.com/'>Offical Website</a>, or <a href='https://sourceforge.net/projects/skfans/'>SourceForge</a></p>
         <h1>Game Engines</h1>
         <p><a href='https://developer.android.com/studio/index.html'>Android Studio</a></p>
         <p><a href='https://www.youtube.com/watch?v=OojQitoAEXs&list=PL2xjPbQaM7JZ_FmXwTAesiAciHEPlGmiW'>Game Tutorials for Android Studio</a></p>
         <p><a href='https://github.com/TheMightyFingers/mightyeditor'>Mighty Editor</a> includes Phaser in a easier interface</p>
         <p><a href='https://www.youtube.com/watch?v=ngvtZZGbwaI'>13 free Open Source software to make your games!</a></p>
-        <p><a href='http://cocos2d.org/'>Cocos2D</a></p>
+        <p><a href='http://cocos2d.org/'>Cocos2D</a> and <a href='https://github.com/cocos2d'>Cocos 2d Github</a></p>
         <p><a href='http://game-editor.com/Main_Page'>Game Editor</a></p>
         <p><a href='https://www.defold.com/'>Defold</a></p>
-        <p><a href='http://www.openfl.org/'></a></p>
+        <p><a href='http://www.openfl.org/'>OpenFL</a></p>
         <p><a href='http://www.clickteam.com/download-centre/retired-products'>The Games Factory 1.06 & More</a></p>
         <p><a href='http://www.clickteam.com/'>Click Team Fusion</a></p>
         <p><a href='https://xenko.com/'>Xenko</a></p>
+        <p><a href='http://www.adventuregamestudio.co.uk/'>Adventure Game Studio:</a> is a great for Adventure Genera games such as King's Quest gameplay or CartLife</p>
         <p>This is a interactice Choose your own adventures to create games on multiple platforms: <a href='https://www.renpy.org/'>Ren'Py</a></p>
         <p>A basic Choose Your Own Adventures with only text and multiple choices to make: <a href='https://www.choiceofgames.com/make-your-own-games/choicescript-intro/'>ChoiceScript Intro</a> and here is the <a href='https://github.com/dfabulich/choicescript'>GitHub</a> Repository</p>
         <h1>Game Art Tools:</h1>
         <p>A Pixel Art tool: <a href='https://www.aseprite.org/older-versions/'>Aseprite</a></p>
         <p><a href='https://www.designer.io/'>Gravit Designer</a> and the <a href='https://www.youtube.com/watch?v=eIgcwY_DGCo'>YouTube-Short-Tutorial(8:58)</a>.</p>
         <p><a href='https://krita.org/en/'>Krita</a></p>
+        <h1>Old Operating Systems:</h1>
+        <p><a href='https://developer.microsoft.com/en-us/microsoft-edge/tools/vms/'>Curently Windows 7, 8, & 10 Virtual Machine links</a></p>
+        <p><a href='https://winworldpc.com/library/operating-systems'>Win World PC</a></p>
+        <h1>Soruces:</h1>
+        <p><a href='https://en.wikipedia.org/wiki/List_of_freeware_video_games'>List of Freeware Video Games (Wikipedia List)</a></p>
+        <p><a href='https://en.wikipedia.org/wiki/List_of_free_and_open-source_software_packages'>List of Free and Open-Source Software Packages</a></p>
+        <p><a href='https://en.wikipedia.org/wiki/List_of_commercial_video_games_released_as_freeware'>List Of COmmercial Video Games Released As Freeware</a></p>
+        <p></p>
       </div>
     );
   }
