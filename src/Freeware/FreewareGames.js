ced7d098c65696a01796c9e3e05956c4bc855188
KB-Spanish-Castle <kylegrantb@gmail.com>
2018-01-23 20:06:18 -0700

    More links of Game Engines Added

--- src/Freeware/FreewareGames.js
+++ src/Freeware/FreewareGames.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './Freeware.css';
+import GameEngines from '../Game-Engines/Game-Engines';
 class FrewareGames extends Component {
   render() {
     return (
@@ -13,6 +14,7 @@ class FrewareGames extends Component {
         <p><a href='http://www.indiedb.com/'>IndieDB</a>: are free Games</p>
         <p><a href='https://www.artsoft.org/'>Rocks N' Diamonds</a></p>
         <p><a href='http://scummvm.org/downloads/'>Multiple Platforms</a> to work with multiple <a href='http://scummvm.org/games/'>Free Games</a></p>
+        <p>King's Quest <a href='http://www.agdinteractive.com/games/games.html'>Remakes</a></p>
         <p><a href='http://www.raceintospace.org/'>Race Into Space</a></p>
         <p><a href='http://forum.caravelgames.com/downloads.php'>Deadly Rooms Of Death Architect Edition</a></p>
         <p>Free Blizzard Games including StarCraft Legacy for: <a href='https://us.battle.net/account/download/?show=classic'>Windows & Mac</a></p>
@@ -26,51 +28,27 @@ class FrewareGames extends Component {
         <p><a href='https://osgameclones.com/'>Free Inspired Game Projects</a></p>
         <h1>Games that were Commerical Became Offical Freeware</h1>
         <p><a href='https://archive.org/details/cart-life-v1.6'>CartLife</a> and the source code at: <a href='https://github.com/gondur/cartlife_src'>GitHub</a></p>
-        <p><a href='https://github.com/akarnokd/open-ig'>Open Imperium Galactica</a></p>
-        <p><a href='http://www.amuletsandarmor.com/'>Amulets & Armor</a></p>
+        <p><a href='https://github.com/akarnokd/open-ig'>Open Imperium Galactica</a>: is a Jave remake with permission from the company.</p>
+        <p><a href='http://www.amuletsandarmor.com/'>Amulets & Armor</a></p>        
         <p>Because of the early Command & Conquer games were released for free a project was made with an easier interface: <a href='http://www.openra.net/'>Open Red Alert</a></p>
         <p>Additonal Links for offial released <a href='https://cncnz.com/features/freeware-classic-command-conquer-games/'>Freeware Classics</a> and <a href='https://cncnet.org/'>Mods</a></p>
         <p><a href='https://www.tribesuniverse.com/'>Tribes Series</a></p>
         <p><a href='http://en.uesp.net/wiki/Arena:Files'>Elder Scrolls: Arena</a></p>
         <p><a href='http://en.uesp.net/wiki/Daggerfall:Files'>Elder Scrolls II: Daggerfall</a></p>
         <p>7 Kingdomes Ancient Adversaries is a realtime strategy mixed with civilzation mechanics including researching: <a href='https://github.com/the3dfxdude/7kaa/releases'>GitHub</a> <a href='https://7kfans.com/'>Offical Website</a>, or <a href='https://sourceforge.net/projects/skfans/'>SourceForge</a></p>
-        <p>King's Quest </p>
         <h1>Open Source Freeware:</h1>
         <p>Liero is a 2D real time version of Worms <a href='http://www.liero.be/'>Liero Offical Website</a> or <a href='https://github.com/gliptic/liero'>GitHub</a></p>
         <p>A good fork to Liero with Linux/Mac/Windows ports is:<a href='http://www.openlierox.net/downloads/'>Open Liero X</a> or at <a href='https://github.com/albertz/openlierox'>GitHub</a></p>
         <p>A puzzle that involves matching and not getting trap <a href='http://www.biniax.com/'>Biniax 2</a></p>
         <p>Free Orion <a href='http://www.freeorion.org/index.php/Main_Page'>Home</a> or <a href='https://github.com/freeorion/freeorion'>GitHub</a></p>
-       <p><a href='https://github.com/teeworlds/teeworlds'>Tee Worlds</a> <a href='https://www.teeworlds.com/'>Offical Website</a></p>
+        <p><a href='https://github.com/teeworlds/teeworlds'>Tee Worlds</a> <a href='https://www.teeworlds.com/'>Offical Website</a></p>
         <p> <a href='https://www.openttd.org/en/'>Open Transport Tycoon Deluxe</a></p>
-        <p><a href='https://github.com/doublespeakgames/adarkroom'>A Dark Room Github</a></p>        
-        <h1>Game Engines</h1>
-        <p><a href='https://developer.android.com/studio/index.html'>Android Studio</a></p>
-        <p><a href='https://www.youtube.com/watch?v=OojQitoAEXs&list=PL2xjPbQaM7JZ_FmXwTAesiAciHEPlGmiW'>Game Tutorials for Android Studio</a></p>
-        <p><a href='https://github.com/TheMightyFingers/mightyeditor'>Mighty Editor</a> includes Phaser in a easier interface</p>
-        <p><a href='https://www.youtube.com/watch?v=ngvtZZGbwaI'>13 free Open Source software to make your games!</a></p>
-        <p><a href='http://cocos2d.org/'>Cocos2D</a> and <a href='https://github.com/cocos2d'>Cocos 2d Github</a></p>
-        <p><a href='http://game-editor.com/Main_Page'>Game Editor</a></p>
-        <p><a href='https://www.defold.com/'>Defold</a></p>
-        <p><a href='http://www.openfl.org/'>OpenFL</a></p>
-        <p><a href='http://www.clickteam.com/download-centre/retired-products'>The Games Factory 1.06 & More</a></p>
-        <p><a href='http://www.clickteam.com/'>Click Team Fusion</a></p>
-        <p><a href='https://xenko.com/'>Xenko</a></p>
-        <p><a href='http://www.adventuregamestudio.co.uk/'>Adventure Game Studio:</a> is a great for Adventure Genera games such as King's Quest gameplay or CartLife</p>
-        <p><a href='https://github.com/frogatto/frogatto'>A Platfrom Engine</a></p>
-        <p><a href='http://jmonkeyengine.org/'>JMonkeyEngine</a></p>
-        <p>This is a interactice Choose your own adventures to create games on multiple platforms: <a href='https://www.renpy.org/'>Ren'Py</a></p>
-        <p>A basic Choose Your Own Adventures with only text and multiple choices to make: <a href='https://www.choiceofgames.com/make-your-own-games/choicescript-intro/'>ChoiceScript Intro</a> and here is the <a href='https://github.com/dfabulich/choicescript'>GitHub</a> Repository</p>
-        <h1>Game Art Tools:</h1>
-        <p>A Pixel Art tool: <a href='https://www.aseprite.org/older-versions/'>Aseprite</a></p>
-        <p><a href='https://www.designer.io/'>Gravit Designer</a> and the <a href='https://www.youtube.com/watch?v=eIgcwY_DGCo'>YouTube-Short-Tutorial(8:58)</a>.</p>
-        <p><a href='https://krita.org/en/'>Krita</a></p>
-        <h1>Old Operating Systems:</h1>
-        <p><a href='https://developer.microsoft.com/en-us/microsoft-edge/tools/vms/'>Curently Windows 7, 8, & 10 Virtual Machine links</a></p>
-        <p><a href='https://winworldpc.com/library/operating-systems'>Win World PC</a></p>
+        <p><a href='https://github.com/doublespeakgames/adarkroom'>A Dark Room Github</a></p>
         <h1>Soruces:</h1>
         <p><a href='https://en.wikipedia.org/wiki/List_of_freeware_video_games'>List of Freeware Video Games (Wikipedia List)</a></p>
         <p><a href='https://en.wikipedia.org/wiki/List_of_free_and_open-source_software_packages'>List of Free and Open-Source Software Packages</a></p>
         <p><a href='https://en.wikipedia.org/wiki/List_of_commercial_video_games_released_as_freeware'>List Of COmmercial Video Games Released As Freeware</a></p>
+        <GameEngines />
       </div>
     );
   }
---
+++ src/Game-Engines/Game-Engines.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './Game-Engines.css';
+class GameEngines extends Component {
+  render() {
+    return (
+      <div className="Freeware-main">
+        <h1>Game Engines</h1>
+        <p><a href='https://developer.android.com/studio/index.html'>Android Studio</a></p>
+        <p><a href='https://www.youtube.com/watch?v=OojQitoAEXs&list=PL2xjPbQaM7JZ_FmXwTAesiAciHEPlGmiW'>Game Tutorials for Android Studio</a></p>
+        <p><a href='https://github.com/TheMightyFingers/mightyeditor'>Mighty Editor</a>: includes Phaser in a easier interface</p>
+        <p><a href='https://www.youtube.com/watch?v=ngvtZZGbwaI'>13 free Open Source software to make your games!</a></p>
+        <p><a href='http://cocos2d.org/'>Cocos2D</a> and <a href='https://github.com/cocos2d'>Cocos 2d Github</a></p>
+        <p><a href='http://game-editor.com/Main_Page'>Game Editor</a></p>
+        <p><a href='https://www.defold.com/'>Defold</a></p>
+        <p><a href='http://www.openfl.org/'>OpenFL</a></p>
+        <p><a href='http://www.clickteam.com/download-centre/retired-products'>The Games Factory 1.06 & More</a></p>
+        <p><a href='http://www.clickteam.com/'>Click Team Fusion</a></p>
+        <p><a href='https://xenko.com/'>Xenko</a></p>
+        <p><a href='http://www.adventuregamestudio.co.uk/'>Adventure Game Studio:</a> is a great for Adventure Genera games such as King's Quest gameplay or CartLife</p>
+        <p><a href='https://github.com/frogatto/frogatto'>A Platfrom Engine</a></p>
+        <p><a href='http://jmonkeyengine.org/'>JMonkeyEngine</a></p>
+        <p><a href='http://www.stencyl.com/'>Stencyl Game Engine</a> it also uses <a url='https://haxe.org/'>Haxe</a> when it comes to coding schripts.</p>
+        <p><a href='https://godotengine.org/'>Godot</a> 2D & 3D Open Source Game Engine</p>
+        <p><a href='https://love2d.org/'>Love Game Engine</a></p>
+        <p><a href='https://playcanvas.com/'>Play Canvas</a></p>
+        <p><a href='https://libgdx.badlogicgames.com/'>libGDX</a></p>
+        <p>This is a interactice Choose your own adventures to create games on multiple platforms: <a href='https://www.renpy.org/'>Ren'Py</a></p>
+        <p>A basic Choose Your Own Adventures with only text and multiple choices to make: <a href='https://www.choiceofgames.com/make-your-own-games/choicescript-intro/'>ChoiceScript Intro</a> and here is the <a href='https://github.com/dfabulich/choicescript'>GitHub</a> Repository</p>
+        <h1>Game Art Tools:</h1>
+        <p>A Pixel Art tool: <a href='https://www.aseprite.org/older-versions/'>Aseprite</a></p>
+        <p><a href='https://www.designer.io/'>Gravit Designer</a> and the <a href='https://www.youtube.com/watch?v=eIgcwY_DGCo'>YouTube-Short-Tutorial(8:58)</a>.</p>
+        <p><a href='https://krita.org/en/'>Krita</a></p>
+        <h1>Old Operating Systems:</h1>
+        <p><a href='https://developer.microsoft.com/en-us/microsoft-edge/tools/vms/'>Curently Windows 7, 8, & 10 Virtual Machine links</a></p>
+        <p><a href='https://winworldpc.com/library/operating-systems'>Win World PC</a></p>
+      </div>
+    );
+  }
+}
+export default GameEngines
