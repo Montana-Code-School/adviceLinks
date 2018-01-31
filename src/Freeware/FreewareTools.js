88ea1219d60d3b024b17c3842c3873697bbf48b4
KB-Spanish-Castle <kylegrantb@gmail.com>
2018-01-31 00:26:08 -0700

    Typos fixed and added PopcornFlix

--- src/Freeware/FreewareTools.js
+++ src/Freeware/FreewareTools.js
@@ -19,9 +19,13 @@ class FreewareTools extends Component {
         <p>Kiwix:</p>
         <p><a href='http://www.kiwix.org/'>Kiwix for Offline Websites including Wikipedia</a></p>
         <p>Kiwix>Download>See All Available Content <a href='http://wiki.kiwix.org/wiki/Content'>All Content Shortcut</a></p>
-        <p>Pluto TV is offcially live free TV with 75+ channels it depends on its revenue from ads.  Alsoe it is on <a href='http://pluto.tv/'>multiple platforms</a> from: Amazon App Store, Roku, Apple Store, Windows Store, Google Play Store, Samsung Store, Sony store for PS3 & PS4.</p>
-        <p>Hint about using Pluto TV on a tablet or phone is to turn the device side ways to show the entire channel schedule.</p>
-        <p><a href='https://www.crackle.com/'>Crackle</a> is owned by Sony and each month the selection changes for what movies are free to watch with adds.</p>
+        <p>Pluto TV is offcially live free TV with 75+ channels it depends on its revenue from ads.</p>
+        <p>Also it is on <a href='http://pluto.tv/'>multiple platforms</a> from: Amazon App Store, Roku, Apple Store on ITunes (Including Apple TV), Xbox Store, Google Play Store, Samsung Store, Sony store for PS3 & PS4.</p>
+        <p>Being in landscape mode for Pluto TV app is full screen.</p>
+        <p>Hint about using Pluto TV on a tablet or phone is to turn the device side ways (Portiat Mode) to show the entire channel schedule.(Be sure to have auto-rotate option enabled)</p>
+        <p>When you have it turned in Portrait Mode and see the schedule at the bottom it shows the Movies & Shows option.  <a href='https://en.wikipedia.org/wiki/Pluto_TV'>(Wikipedia)</a>  It also includes Movies & TV Shows that can be watched any time with less ads than Crackle.</p>
+        <p><a href='https://www.youtube.com/user/ScreenMediaPictures'>Popcorn Flix</a> at <a href='https://en.wikipedia.org/wiki/Popcornflix'>Wikipedia</a> and <a href='https://www.popcornflix.com/'>offical website</a> also have their own apps.</p>
+        <p><a href='https://www.crackle.com/'>Crackle</a> is owned by Sony and each month the selection changes for what movies are free to watch with ads.</p>
         <p><a href='http://publicdomainmovies.net/'>Public Domain Movies</a></p>
         <p><a href='https://librivox.org/'>LibriVox</a>: Free public domain works adapted into free Audio Books</p>
         <FreewareGames/>
